4d1f624da53c746f76e9d2b933112ad348e2b9b9
Torara1998 <minh.dang160205@vnuk.edu.vn>
2018-01-16 11:42:18 +0700

    end PTBN

---
+++ PhuongTringBacNhat/app/src/main/java/CSE16/DangHieuMinh/phuongtringbacnhat/MainActivity.java
@@ -0,0 +1,13 @@
+package CSE16.DangHieuMinh.phuongtringbacnhat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
